da9831c617ead4523ed782de6eb47d0963ee6f77
Marta Jiménez Franco <jimenez.franco.marta@gmail.com>
2013-11-02 19:56:45 +0100

    Full screen and no title

--- Polygons/Polygons/src/main/java/com/github/polygons/MainActivity.java
+++ Polygons/Polygons/src/main/java/com/github/polygons/MainActivity.java
@@ -10,12 +10,17 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE); 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         if (savedInstanceState == null) {
