c9b3ebed7fd2ffe0b8abf636530b7b8a528b2520
Marta Jiménez Franco <jimenez.franco.marta@gmail.com>
2013-11-03 03:11:31 +0100

    Remove unnecessary imports

--- Polygons/Polygons/src/main/java/com/github/polygons/MainActivity.java
+++ Polygons/Polygons/src/main/java/com/github/polygons/MainActivity.java
@@ -1,16 +1,14 @@
 package com.github.polygons;
 
 import android.content.Context;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
