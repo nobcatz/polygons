6680606d5d5769a7b264b79732f7a1a508c10659
Marta Jiménez Franco <tetaliuss@gmail.com>
2013-11-05 13:39:05 +0100

    remove unused imports

--- Polygons/Polygons/src/main/java/com/github/polygons/MainActivity.java
+++ Polygons/Polygons/src/main/java/com/github/polygons/MainActivity.java
@@ -9,8 +9,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
