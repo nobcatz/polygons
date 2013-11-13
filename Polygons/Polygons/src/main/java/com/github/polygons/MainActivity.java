f09e224d7beb52ec8e0785ad73e7e6d680fec10b
Marta Jiménez Franco <tetaliuss@gmail.com>
2013-11-13 15:52:22 +0100

    Delete fragments from game
    They were complicated :(

--- Polygons/Polygons/src/main/java/com/github/polygons/MainActivity.java
+++ Polygons/Polygons/src/main/java/com/github/polygons/MainActivity.java
@@ -1,20 +1,13 @@
 package com.github.polygons;
 
+import android.app.Activity;
 import android.content.Context;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,71 +16,8 @@ public class MainActivity extends ActionBarActivity {
         setRequestedOrientation(getResources().getInteger(R.integer.orientation));
         setContentView(R.layout.activity_main);
 
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            View circle = new Circle(rootView.getContext());
-            RelativeLayout rl = (RelativeLayout) rootView.findViewById(R.id.relative_layout_test);
-
-            circle.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    //Hay que cambiarlo porque el context sale con el fonde del color del tema en vez de negro
-                    Context context = v.getContext();
-                    CharSequence text = "I am dynamic";
-                    int duration = Toast.LENGTH_SHORT;
-
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();
-                }
-            }
-            );
-
-            rl.addView(circle);
-            return rootView;
-        }
-    }
 
     public void newGame(View v) {
 
@@ -97,7 +27,5 @@ public class MainActivity extends ActionBarActivity {
 
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
-
     }
-
 }
