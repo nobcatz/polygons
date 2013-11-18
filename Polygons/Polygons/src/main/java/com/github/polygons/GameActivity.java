46934a75c5675e14caa047665182e0992226f686
Marta Jiménez Franco <tetaliuss@gmail.com>
2013-11-18 19:27:20 +0100

    Views in .xml changed automatically

--- Polygons/Polygons/src/main/java/com/github/polygons/GameActivity.java
+++ Polygons/Polygons/src/main/java/com/github/polygons/GameActivity.java
@@ -15,5 +15,22 @@ public class GameActivity extends Activity{
         //noinspection MagicConstant
         setRequestedOrientation(getResources().getInteger(R.integer.orientation));
         setContentView(R.layout.activity_game);
+    public void generateFigures(String layoutNum, int figureNum){
+        Resources res = getResources();
+        int idfigure = res.getIdentifier("figure_" + layoutNum + "_" + figureNum, "id", getApplicationContext().getPackageName());
+        int idLayout= res.getIdentifier("linear_layout_"+layoutNum, "id", getApplicationContext().getPackageName());
+
+
+        Figure figure = (Figure)findViewById(idfigure);
+        LinearLayout ll = (LinearLayout)findViewById(idLayout);
+
+        ViewGroup.LayoutParams lp = figure.getLayoutParams();
+        figure.setVisibility(View.GONE);
+
+        figure = new Oval(getApplicationContext());
+        figure.setVisibility(View.VISIBLE);
+        figure.setLayoutParams(lp);
+        ll.addView(figure);
+    }
     }
 }
