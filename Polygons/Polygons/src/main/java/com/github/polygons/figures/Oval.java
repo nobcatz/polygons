69fcc3439b7ce20857cad1597f7cd55c76f1d3c0
Marta Jiménez Franco <jimenez.franco.marta@gmail.com>
2013-11-19 00:04:37 +0100

    created outside line

--- Polygons/Polygons/src/main/java/com/github/polygons/figures/Oval.java
+++ Polygons/Polygons/src/main/java/com/github/polygons/figures/Oval.java
@@ -2,8 +2,11 @@ package com.github.polygons.figures;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
+
+import com.github.polygons.R;
 
 /**
  * Created by Marta on 03/11/2013.
@@ -27,5 +30,9 @@ public class Oval extends Figure {
 
     public void drawFigure(Canvas canvas) {
         canvas.drawOval(new RectF(0, 0, canvas.getWidth(), canvas.getHeight()), this.paint);
+
+        paint.setColor(getResources().getColor(R.color.base3));
+
+        canvas.drawOval(new RectF(10,10, canvas.getWidth()-10, canvas.getHeight()-10), this.paint);
     }
 }
