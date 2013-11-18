bab8df29d94158a0a6f92a82de5f70714e0a149d
Marta Jiménez Franco <tetaliuss@gmail.com>
2013-11-19 00:40:56 +0100

    create logic with levels

--- Polygons/Polygons/src/main/java/com/github/polygons/logic/Keeper.java
+++ Polygons/Polygons/src/main/java/com/github/polygons/logic/Keeper.java
@@ -1,5 +1,10 @@
 package com.github.polygons.logic;
 
+import android.content.Intent;
+
+import com.github.polygons.FigureListActivity;
+import com.github.polygons.GameActivity;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,11 +12,14 @@ import java.util.List;
  * Created by Marta on 18/11/2013.
  */
 public class Keeper {
+
+    private int level;
     private static Keeper instance = null;
 
     private List<Integer> figuresId = new ArrayList<Integer>();
 
     private Keeper() {
+        this.level = 1;
     }
 
     public static Keeper getInstance() {
@@ -22,7 +30,22 @@ public class Keeper {
         return instance;
     }
 
+    public void levelUp(){
+        this.level++;
+    }
+
     public void addFigureId(int id){
         figuresId.add(id);
     }
+
+    public boolean isClickCorrect(int idClicking){
+        return figuresId.get(0) == idClicking;
+    }
+
+    public boolean removeFirstFigure(){
+        figuresId.remove(0);
+        return figuresId.isEmpty();
+    }
+
+
 }
