e33fd95dc44bb8b3aa276f41f113815adf75f937
Marta Jiménez Franco <tetaliuss@gmail.com>
2013-11-18 19:54:19 +0100

    IDs added to figures

--- Polygons/Polygons/src/main/java/com/github/polygons/figures/Square.java
+++ Polygons/Polygons/src/main/java/com/github/polygons/figures/Square.java
@@ -13,18 +13,23 @@ public class Square extends Figure{
 
     public Square(Context context) {
         super(context);
+        this.id = 1;
     }
 
     public Square(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.id = 0;
     }
 
     public Square(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        this.id = 0;
     }
 
     @Override
     public void drawFigure(Canvas canvas) {
         canvas.drawRect(0, 0, canvas.getWidth(), canvas.getWidth(), this.paint);
     }
+
+
 }
