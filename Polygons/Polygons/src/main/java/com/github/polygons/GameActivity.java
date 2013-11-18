d897bb20b45c7a6b8790e80353ff7caa5ce9ffe6
Marta Jiménez Franco <jimenez.franco.marta@gmail.com>
2013-11-18 20:09:40 +0100

    Created GamActivity with its layout

---
+++ Polygons/Polygons/src/main/java/com/github/polygons/GameActivity.java
@@ -0,0 +1,16 @@
+package com.github.polygons;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+
+    }
+
+}
