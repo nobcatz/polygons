d1383997bbec63d4bfbc1582d6f54f7a331e6c79
Marta Jiménez Franco <tetaliuss@gmail.com>
2013-11-19 00:58:13 +0100

    Animation level test

--- Polygons/Polygons/src/main/java/com/github/polygons/LevelActivity.java
+++ Polygons/Polygons/src/main/java/com/github/polygons/LevelActivity.java
@@ -2,6 +2,10 @@ package com.github.polygons;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class LevelActivity extends ActionBarActivity {
 
@@ -9,6 +13,24 @@ public class LevelActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level);
+
+        startAnimations();
+           
+
+    }
+
+    private void startAnimations() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        LinearLayout l=(LinearLayout) findViewById(R.id.lin_lay);
+        l.clearAnimation();
+        l.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        TextView tv = (TextView) findViewById(R.id.logo);
+        tv.clearAnimation();
+        tv.startAnimation(anim);
 
     }
 
