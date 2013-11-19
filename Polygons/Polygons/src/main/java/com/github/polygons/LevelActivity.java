6312c7a0894694d7c9441af554e39890c36c8c84
Marta Jiménez Franco <tetaliuss@gmail.com>
2013-11-19 01:25:51 +0100

    level activity with animation

--- Polygons/Polygons/src/main/java/com/github/polygons/LevelActivity.java
+++ Polygons/Polygons/src/main/java/com/github/polygons/LevelActivity.java
@@ -1,21 +1,23 @@
 package com.github.polygons;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class LevelActivity extends ActionBarActivity {
+import com.github.polygons.logic.Keeper;
+
+public class LevelActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level);
-
         startAnimations();
-           
+
 
     }
 
@@ -28,10 +30,37 @@ public class LevelActivity extends ActionBarActivity {
 
         anim = AnimationUtils.loadAnimation(this, R.anim.translate);
         anim.reset();
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                createNewActivity();
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
         TextView tv = (TextView) findViewById(R.id.logo);
+        String level = getResources().getString(R.string.level) +" " + Keeper.getInstance().getLevel();
+        tv.setText(level);
         tv.clearAnimation();
         tv.startAnimation(anim);
 
+
+    }
+
+    private void createNewActivity(){
+        Intent intent = new Intent(this, FigureListActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(intent);
+        this.finish();
     }
 
 }
