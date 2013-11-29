8eec47a3dbdae40ff1a29713ecee67aea341d9ae
Marta Jiménez Franco <jimenez.franco.marta@gmail.com>
2013-11-29 14:01:05 +0100

    Activity when losing game

---
+++ Polygons/Polygons/src/main/java/com/github/polygons/LostActivity.java
@@ -0,0 +1,23 @@
+package com.github.polygons;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class LostActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lost);
+    }
+
+}
