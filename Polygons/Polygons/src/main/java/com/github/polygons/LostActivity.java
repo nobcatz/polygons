f5f49cc9e6ba5ab6bc91fc10f21757bed292c071
Marta Jiménez Franco <jimenez.franco.marta@gmail.com>
2013-12-03 18:42:14 +0100

    Retry and exit button when losing

--- Polygons/Polygons/src/main/java/com/github/polygons/LostActivity.java
+++ Polygons/Polygons/src/main/java/com/github/polygons/LostActivity.java
@@ -1,7 +1,9 @@
 package com.github.polygons;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.github.polygons.logic.Keeper;
 import com.nobcatz.android.polygons.R;
@@ -13,6 +15,25 @@ public class LostActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lost);
         Keeper.getInstance().lostGame();
+    }
+
+    @Override
+    public void onBackPressed() {/*Do nothing*/}
+
+
+    public void exit(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(intent);
+        this.finish();
+    }
+
+    public void retry(View v){
+
+        Intent intent = new Intent(this, LevelActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(intent);
+
     }
 
 }
