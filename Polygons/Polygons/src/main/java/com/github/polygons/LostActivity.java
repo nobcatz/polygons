96a9816adebc66aaf13da0f222ab7dd9357112c3
Marta Jiménez Franco <tetaliuss@gmail.com>
2013-12-03 18:11:20 +0100

    Fading when losing game

--- Polygons/Polygons/src/main/java/com/github/polygons/LostActivity.java
+++ Polygons/Polygons/src/main/java/com/github/polygons/LostActivity.java
@@ -1,16 +1,7 @@
 package com.github.polygons;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
 import com.nobcatz.android.polygons.R;
 
