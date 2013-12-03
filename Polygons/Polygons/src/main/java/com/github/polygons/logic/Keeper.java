f9a679a4c42913a6264426baa23f83b6172bfa42
Marta Jiménez Franco <tetaliuss@gmail.com>
2013-12-03 18:17:27 +0100

    reset levels when losing

--- Polygons/Polygons/src/main/java/com/github/polygons/logic/Keeper.java
+++ Polygons/Polygons/src/main/java/com/github/polygons/logic/Keeper.java
@@ -30,30 +30,24 @@ public class Keeper {
         return instance;
     }
 
-    public void levelUp(){
-        this.level++;
-    }
-
-    public void addFigureId(int id){
-        figuresId.add(id);
-    }
-
-    public boolean isClickCorrect(int idClicking){
-        return figuresId.get(0) == idClicking;
-    }
-
     public boolean removeFirstFigure(){
         figuresId.remove(0);
         return figuresId.isEmpty();
     }
 
-    public void resetFigures(){
-        figuresId.clear();
+    public void lostGame(){
+        instance = null;
     }
+
+    public void levelUp(){this.level++;}
+
+    public void addFigureId(int id){figuresId.add(id);}
+
+    public boolean isClickCorrect(int idClicking){return figuresId.get(0) == idClicking;}
+
+    public void resetFigures(){figuresId.clear();}
 
     public List<Integer> getFiguresId(){return this.figuresId;}
     
-    public int getLevel() {
-        return level;
-    }
+    public int getLevel() {return level;}
 }
