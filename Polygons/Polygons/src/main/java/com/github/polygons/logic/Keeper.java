7aca014dcdcd804e7695c07cd113ac99c8b86112
Marta Jiménez Franco <tetaliuss@gmail.com>
2013-12-04 12:24:14 +0100

    More figures in X levels

--- Polygons/Polygons/src/main/java/com/github/polygons/logic/Keeper.java
+++ Polygons/Polygons/src/main/java/com/github/polygons/logic/Keeper.java
@@ -15,6 +15,7 @@ public class Keeper {
 
     private int level;
     private static Keeper instance = null;
+    private int layouts;
 
     private List<Integer> figuresId = new ArrayList<Integer>();
 
@@ -29,24 +30,61 @@ public class Keeper {
         return instance;
     }
 
-    public boolean removeFirstFigure(){
+    public boolean removeFirstFigure() {
         figuresId.remove(0);
         return figuresId.isEmpty();
     }
 
-    public void reset(){
+    public void reset() {
         instance = null;
     }
 
-    public void levelUp(){this.level++;}
+    public void levelUp() {
+        this.level++;
+    }
 
-    public void addFigureId(int id){figuresId.add(id);}
+    public void addFigureId(int id) {
+        figuresId.add(id);
+    }
 
-    public boolean isClickCorrect(int idClicking){return figuresId.get(0) == idClicking;}
+    public boolean isClickCorrect(int idClicking) {
+        return figuresId.get(0) == idClicking;
+    }
 
-    public void resetFigures(){figuresId.clear();}
+    public void resetFigures() {
+        figuresId.clear();
+    }
 
-    public List<Integer> getFiguresId(){return this.figuresId;}
-    
-    public int getLevel() {return level;}
+    public List<Integer> getFiguresId() {
+        return this.figuresId;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getLayoutNumber() {
+        generateLayoutNumber();
+        return this.layouts;
+    }
+
+    private void generateLayoutNumber() {
+        switch (level) {
+            case 1:
+                this.layouts = 1;
+                break;
+            case 5:
+                this.layouts = 2;
+                break;
+            case 10:
+                this.layouts = 3;
+                break;
+            case 20:
+                this.layouts = 4;
+                break;
+            case 30:
+                this.layouts = 5;
+                break;
+        }
+    }
 }
